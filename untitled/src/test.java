11528a38e8ca1c60790ba66a5c390d035a5ac767
aliiimaher <alimohammadmaher@gmail.com>
2026-03-27 14:15:16 -0400

    for loop approach

--- untitled/src/test.java
+++ untitled/src/test.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class test {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        for (int number : numbers) {
+            System.out.println("Number: " + number);
+        }
+    }
 }
